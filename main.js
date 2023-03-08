6bfdd99faf09e3409828db74979dbd39fed46496
amisu-ctr <hamisurahman@gmail.com>
2023-03-08 00:48:12 +0000

    Create a newCard() function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let firstCard = 10;
 let secondCard = 7;
 let sum = firstCard + secondCard;
+let cards = [firstCard, secondCard]
 let hasBlackJack = false;
 let isAlive = true
 let message = ""
@@ -8,8 +9,15 @@ const messageEl = document.getElementById('message-el')
 const sumEl = document.getElementById('sum-el')
 const cardsEl = document.getElementById("cards-el")
 
-
 function startGame() {
+    renderGame();
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for(i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + ' ';
+    }
     sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = 'Do you want to draw a new card?';
@@ -24,5 +32,11 @@ function startGame() {
       messageEl.textContent = message
 }
 
+function newCard() {
+    let card = 6;
+    sum += card
+    cards.push(card)
+    renderGame()
+}
 
 
