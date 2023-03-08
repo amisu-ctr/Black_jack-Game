d0c823eaf90693824b01ece7189fe0eb2992c239
amisu-ctr <hamisurahman@gmail.com>
2023-03-08 01:47:05 +0000

    Refactored startGame() functions

--- main.js
+++ main.js
@@ -1,42 +1,57 @@
-let firstCard = 10;
-let secondCard = 7;
-let sum = firstCard + secondCard;
-let cards = [firstCard, secondCard]
+let sum = 0;
+let cards = [];
 let hasBlackJack = false;
-let isAlive = true
-let message = ""
-const messageEl = document.getElementById('message-el')
-const sumEl = document.getElementById('sum-el')
-const cardsEl = document.getElementById("cards-el")
+let isAlive = true;
+let message = '';
+const messageEl = document.getElementById('message-el');
+const sumEl = document.getElementById('sum-el');
+const cardsEl = document.getElementById('cards-el');
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
 
 function startGame() {
-    renderGame();
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: "
-    for(i = 0; i < cards.length; i++){
-        cardsEl.textContent += cards[i] + ' ';
-    }
-    sumEl.textContent = "Sum: " + sum;
-    if (sum <= 20) {
-        message = 'Do you want to draw a new card?';
-      } else if (sum === 21) {
-        message = 'Whoo! youve got Blackjack!'
-        hasBlackJack = true;
-      } else {
-        message = "you're out of the game!"
-        isAlive = false
-      }
+  cardsEl.textContent = 'Cards: ';
+  for (i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + ' ';
+  }
+  sumEl.textContent = 'Sum: ' + sum;
+  if (sum <= 20) {
+    message = 'Do you want to draw a new card?';
+  } else if (sum === 21) {
+    message = 'Whoo! youve got Blackjack!';
+    hasBlackJack = true;
+  } else {
+    message = "you're out of the game!";
+    isAlive = false;
+  }
 
-      messageEl.textContent = message
+  messageEl.textContent = message;
 }
 
 function newCard() {
-    let card = 6;
-    sum += card
-    cards.push(card)
-    renderGame()
+    // only allow player to get a new card if she is alive and does not have a blackjack
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
-
-
